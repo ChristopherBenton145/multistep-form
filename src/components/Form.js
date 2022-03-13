b525d172fed6f6eef841c38cbb20d6c632c26639
TempUsername2436 <christopherpokemon30@gmail.com>
2022-03-13 13:49:12 -0700

    Debuging JavaScript state code

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import "../styles/Form.scss"
+import React, { useState, useRef, useEffect } from 'react';
+import "../styles/Form.scss";
 
 function Form() {
+  let [currentStep, setCurrentStep] = useState(0);
+  let [steps, setSteps] = useState([]);
+  const form = useRef(null);
+
+  useEffect(() => {
+      let localSteps = [];
+      form.current.childNodes.forEach((step, index) => {
+          localSteps.push(step);
+      });
+      setSteps(localSteps);
+    }, []);
+    
+  function previous(e) {
+    setCurrentStep(prevState => prevState - 1);
+  }
+
+  function next(e) {
+    steps[currentStep].classList.toggle("active");
+    steps[currentStep + 1].classList.toggle("active");
+    setCurrentStep(prevState => prevState + 1);
+}
+
   return (
-    <form>
+    <form ref={form}>
         <div className="step active">
             <h3>Account Information</h3>
             <input type="text" name="firstname" placeholder="First Name"></input>
             <input type="text" name="lastname" placeholder="Last Name"></input>
             <input type="text" name="age" placeholder="Age"></input>
             <div className="buttons">
-                <button type="button" className="button-previous">Previous</button>
-                <button type="button" className="button-next">Next</button>
+                <button onClick={(e) => previous(e)} type="button" className="button-previous">Previous</button>
+                <button onClick={(e) => next(e)} type="button" className="button-next">Next</button>
             </div>
         </div>
         <div className="step">
@@ -20,8 +42,8 @@ function Form() {
             <input type="text" name="lastname" placeholder="Last Name"></input>
             <input type="text" name="age" placeholder="Age"></input>
             <div className="buttons">
-                <button type="button" className="button-next">Next</button>
-                <button type="button" className="button-previous">Previous</button>
+                <button onClick={(e) => previous(e)} type="button" className="button-previous">Previous</button>
+                <button onClick={(e) => next(e)} type="button" className="button-next">Next</button>
             </div>
         </div>
         <div className="step">
@@ -30,8 +52,8 @@ function Form() {
             <input type="text" name="lastname" placeholder="Last Name"></input>
             <input type="text" name="age" placeholder="Age"></input>
             <div className="buttons">
-                <button type="button" className="button-next">Next</button>
-                <button type="button" className="button-previous">Previous</button>
+                <button onClick={(e) => previous(e)} type="button" className="button-previous">Previous</button>
+                <button onClick={(e) => next(e)} type="button" className="button-next">Next</button>
             </div>
         </div>
     </form>
